337997f0027f1eaf04cec9d95ccf3bb17276a79b
Diego <rijmjada@gmail.com>
2024-02-26 14:40:55 -0300

    up

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Obtiene la parte de la URL después de la última barra
-    var currentPage = window.location.pathname.split('/').pop();
+    
+    let currentPage = window.location.pathname.split('/').pop();
+    let navLinks = document.querySelectorAll('.navbar-nav .nav-link');
 
-    // Obtiene todos los elementos de enlace en la barra de navegación
-    var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
-
-    // Elimina la clase 'active' de todos los enlaces
     navLinks.forEach(function (link) {
         link.classList.remove('active');
     });
 
-    // Itera sobre los elementos de enlace y establece la clase 'active' si la URL coincide
     navLinks.forEach(function (link) {
-        var linkHref = link.getAttribute('href');
-
-        // Verifica si estamos en la página de inicio o si la URL coincide
+        let linkHref = link.getAttribute('href');
         if ((currentPage === '' && linkHref === './') || (currentPage !== '' && linkHref.includes(currentPage))) {
             link.classList.add('active');
         }
     });
+
 });
