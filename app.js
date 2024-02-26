01253d4ed1620ae3c911ad76a9511326a5b0a0d1
Diego <rijmjada@gmail.com>
2024-02-26 13:29:15 -0300

    up

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-    document.addEventListener('DOMContentLoaded', function () {
-        // Obtiene la URL de la página actual
-        var currentPage = window.location.pathname.split('/').pop();
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtiene la parte de la URL después de la última barra
+    var currentPage = window.location.pathname.split('/').pop();
 
-        // Obtiene todos los elementos de enlace en la barra de navegación
-        var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    // Obtiene todos los elementos de enlace en la barra de navegación
+    var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
 
-        // Elimina la clase 'active' de todos los enlaces
-        navLinks.forEach(function (link) {
-            link.classList.remove('active');
-        });
-
-        // Itera sobre los elementos de enlace y establece la clase 'active' si la URL coincide
-        navLinks.forEach(function (link) {
-            var linkHref = link.getAttribute('href');
-
-            if (linkHref && linkHref.endsWith(currentPage)) {
-                link.classList.add('active');
-            }
-        });
+    // Elimina la clase 'active' de todos los enlaces
+    navLinks.forEach(function (link) {
+        link.classList.remove('active');
     });
+
+    // Itera sobre los elementos de enlace y establece la clase 'active' si la URL coincide
+    navLinks.forEach(function (link) {
+        var linkHref = link.getAttribute('href');
+
+        if (linkHref && currentPage && linkHref.includes(currentPage)) {
+            link.classList.add('active');
+        }
+    });
+});
